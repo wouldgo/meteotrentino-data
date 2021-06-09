4485a686c9607d1df00413e4313e020e72b9d73c
wouldgo <wouldgo84@gmail.com>
2021-06-10 00:09:01 +0200

    adding pressure

--- src/api/meteotrentino.js
+++ src/api/meteotrentino.js
@@ -1,6 +1,7 @@
 'use strict';
 const parser = require('xml2json')
   , zonedTimeToUtc = require('date-fns-tz/zonedTimeToUtc')
+  , parse = require('date-fns/parse')
   , isSameMinute = require('date-fns/isSameMinute')
   , parserOptions = {
     'object': true,
@@ -62,11 +63,32 @@ const parser = require('xml2json')
           };
         }
       }
+    }
+  , parsePressure = data => {
+      const {html} = parser.toJson(data, parserOptions)
+        , jsBloat = html.head.script[2].$t
+        , unbloated = jsBloat.split(/barValues=(.+],]);/)[1]
+            .replace(/\'/gi, '"')
+            .replace('],]', ']]')
+        , datas = JSON.parse(unbloated)
+        , toReturn = [];
+
+      for (const [stringyData, value] of datas) {
+        const date = zonedTimeToUtc(parse(stringyData, 'd/M \'h.\'H:m', new Date()), 'Europe/Rome');
+
+        toReturn.push({
+          'timestamp': date,
+          'measure': 'hPa',
+          value
+        });
+      }
+      return toReturn;
     };
 
 module.exports = async function meteoTrentinoModule({Client, checkCode, log, meteotrentino}) {
   const {stations} = meteotrentino
     , client = new Client('http://dati.meteotrentino.it')
+    , contentMeteotrentinoClient = new Client('https://content.meteotrentino.it')
     , weatherStationData = async function weatherStationData(code) {
         log.debug('GET weather station data');
         const {statusCode, body} = await client.request({
@@ -95,6 +117,32 @@ module.exports = async function meteoTrentinoModule({Client, checkCode, log, met
         log.debug('GET humidex data response parsed');
 
         return data;
+      }
+    , pressureData = async function pressureData(code) {
+        log.debug('GET pressure data');
+        let thisStats = [];
+
+        try {
+          const {statusCode, body} = await contentMeteotrentinoClient.request({
+            'path': `/dati-meteo/stazioni/grafici/BAR.aspx?id=${code}`,
+            'method': 'GET'
+          });
+
+          log.debug('GET pressure data returns');
+
+          const data = await checkCode(body, statusCode);
+
+          thisStats = parsePressure(data);
+          log.debug('GET pressure data response parsed');
+
+          return thisStats;
+        } catch (err) {
+
+          log.error(err, 'GET pressure data went in error');
+        } finally {
+
+          return timestamp => thisStats.find(elm => isSameMinute(elm.timestamp, timestamp));
+        }
       };
 
   if (stations.length === 0) {
@@ -109,13 +157,15 @@ module.exports = async function meteoTrentinoModule({Client, checkCode, log, met
 
       for (const {code, name} of stations) {
         const {rains, radiations, temperatures, humidity} = await weatherStationData(code)
+          , pressures = await pressureData(code)
           , {'timestamp': timestampHumidex, ...restHumidex} = parseHumidex(name, actualHumidexData);
 
         for (let index = 0; index < temperatures.length; index += 1) {
           const {timestamp, ...restTemp} = temperatures[index]
             , {'timestamp': timestampRains, ...restRains} = rains[index]
             , {'timestamp': timestampRadiations, ...restRadiations} = radiations[index]
-            , {'timestamp': timestampMoisture, ...restHumidity} = humidity[index];
+            , {'timestamp': timestampMoisture, ...restHumidity} = humidity[index]
+            , pressureDataPoint = pressures(timestamp);
 
           if (isSameMinute(timestamp, timestampRains) &&
             isSameMinute(timestampRains, timestampRadiations) &&
@@ -134,6 +184,12 @@ module.exports = async function meteoTrentinoModule({Client, checkCode, log, met
               toPush.humidex = restHumidex;
             }
 
+            if (pressureDataPoint != null) {
+              delete pressureDataPoint.timestamp;
+
+              toPush.pressure = pressureDataPoint;
+            }
+
             statsToReturn.push(toPush);
           } else {
 
@@ -142,9 +198,12 @@ module.exports = async function meteoTrentinoModule({Client, checkCode, log, met
         }
       }
 
-      log.info(`Returning data for ${statsToReturn.length} stations`);
+      log.info(`Returning ${statsToReturn.length} data points`);
       return statsToReturn;
     },
-    'close': () => client.close()
+    'close': () => Promise.all([
+      contentMeteotrentinoClient.close(),
+      client.close()
+    ])
   };
 };
